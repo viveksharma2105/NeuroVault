50f4107d36c3c3bf0dd4105b696bf8c0748490b9
gauriiv <garimasharma4467@gmail.com>
2025-08-24 09:30:37 +0530

    Schema

--- src/db.ts
+++ src/db.ts
@@ -1,35 +1,46 @@
-import mongoose from "mongoose";
-const Schema = mongoose.Schema;
+import mongoose,{ model, Schema } from "mongoose";
 
-const userSchema = new Schema({
-    email:{type:String, require:true},
-    name: String
-})
-
-const userSchema = new Schema({
-    email:{type:String, require:true},
-    name: String
-})
-
-const userSchema = new Schema({
-    email:{type:String, require:true},
-    name: String
-})
-
-const userSchema = new Schema({
-    email:{type:String, require:true},
-    name: String
-})
-
-const userSchema = new Schema({
-    email:{type:String, require:true},
-    name: String
-})
+const UserSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
 
 
-const userModel = mongoose.model("user", userSchema);
 
-module.exports = {
-    userModel,
-    
-}
+// const tagSchema = new Schema({
+//   title: { type: String, required: true, unique: true }
+// });
+
+
+
+
+// const contentTypes = ['image', 'video', 'article', 'audio']; // Extend as needed
+
+// const contentSchema = new Schema({
+//   link: { type: String, required: true },
+//   type: { type: String, enum: contentTypes, required: true },
+//   title: { type: String, required: true },
+//   tags: [{ type: Types.ObjectId, ref: 'Tag' }],
+//   userId: { type: Types.ObjectId, ref: 'User', required: true },
+// });
+
+
+// const linkSchema = new Schema({
+//   hash: { type: String, required: true },
+//   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+// });
+
+
+
+export const UserModel =  model("User", UserSchema);
+// const contentModel =  model("content", contentSchema);
+// const tagModel =   model("tags", tagSchema);
+// const linkModel =  model("link", linkSchema)
+
+// module.exports = {
+//     userModel,
+//     contentModel,
+//     tagModel,
+//     linkModel
+
+// }
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
 import express from "express";
-
+import z from "zod"
 import jwt from "jsonwebtoken"
+import { UserModel } from "./db.js";
 
 const app = express();
+app.use(express.json());
 
-app.post("/api/v1/signup", (req, res) => {
+
+app.post("/api/v1/signup", async (req, res) => {
+    //zod validation and hash the  password
+    const username =req.body.username;
+    const password = req.body.password;
+
+    await UserModel.create({
+        username: username,
+        password: password
+    })
+    res.json({
+        message: "User signed  up"
+    })
+
 
 })
 
