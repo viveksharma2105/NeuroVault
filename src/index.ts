799d1b2e9d6b0f5879e1340a84ed25538f2c64d8
gauriiv <garimasharma4467@gmail.com>
2025-08-27 20:24:25 +0530

    delete endpoint

--- src/index.ts
+++ src/index.ts
@@ -116,7 +116,7 @@ app.get("/api/v1/content",userMiddleware, async (req, res) => {
     })
 })
 
-app.delete("/api/v1/content", async (req, res) => {
+app.delete("/api/v1/content",userMiddleware, async (req, res) => {
     const contentId = req.body.contentId;
     await contentModel.deleteMany({
       contentId,
